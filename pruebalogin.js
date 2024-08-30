e3d68fa15e8f84d6685e3f25e078f0f516c02bb9
Owen Arcega <owenarcega11@gmail.com>
2024-08-30 17:06:30 -0600

    update

--- pruebalogin.js
+++ pruebalogin.js
@@ -2,72 +2,69 @@ document.addEventListener("DOMContentLoaded", () => {
   const registerButton = document.getElementById("register");
   const loginButton = document.getElementById("login");
   const container = document.getElementById("container");
-  
+
   registerButton.addEventListener("click", () => {
-      container.classList.add("right-panel-active");
-    });
-    
-    loginButton.addEventListener("click", () => {
-      container.classList.remove("right-panel-active");
-    });
-  
-const btnReg = document.getElementById("btnRegister")
-    btnReg.addEventListener("click", (e) => {
-        e.preventDefault();
+    container.classList.add("right-panel-active");
+  });
 
-        let newUsername = document.getElementById("newUsername").value;
-        let newEmail = document.getElementById("newEmail").value;
-        let newPassword = document.getElementById("newPassword").value;
+  loginButton.addEventListener("click", () => {
+    container.classList.remove("right-panel-active");
+  });
 
-        fetch("https://nodetest-p2ot.onrender.com/registrarUsuario", {
-          method: "POST",
-          body: JSON.stringify({
-            usuario: newUsername,
-            correo: newEmail,
-            contrasena: newPassword,
-          }),
-          headers: {
-            "Content-Type": "application/json",
-          },
-        })
-          .then((res) => res.json())
-          .then((data) => {
-            if (data.status == "error") {
-              alert("Error: " + data.error);
-            } else {
-              alert("Usuario registrado con éxito!");
-            }
-          });        
+  const btnReg = document.getElementById("btnRegister");
+  btnReg.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    let newUsername = document.getElementById("newUsername").value;
+    let newEmail = document.getElementById("newEmail").value;
+    let newPassword = document.getElementById("newPassword").value;
+
+    fetch("https://nodetest-p2ot.onrender.com/registrarUsuario", {
+      method: "POST",
+      body: JSON.stringify({
+        usuario: newUsername,
+        correo: newEmail,
+        contrasena: newPassword,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
     })
-    
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.status == "error") {
+          alert("Error: " + data.error);
+        } else {
+          alert("Usuario registrado con éxito!");
+        }
+      });
+  });
 
-const btnLog = document.getElementById("btnLogin")
+  const btnLog = document.getElementById("btnLogin");
   btnLog.addEventListener("click", (e) => {
-     e.preventDefault();
-     let username = document.getElementById("username").value;
-     let password = document.getElementById("password").value;
+    e.preventDefault();
+    let username = dxcument.getElementById("username").value;
+    let password = document.getElementById("password").value;
 
-     fetch("https://nodetest-p2ot.onrender.com/login", {
-       method: "POST",
-       body: JSON.stringify({
-         nombre: username,
-         contrasena: password,
-       }),
-       headers: {
-         "Content-Type": "application/json",
-       },
-     })
-       .then((res) => res.json())
-       .then((data) => {
-         if (data.status == "ok") {
-           sessionStorage.setItem("userId", data.body[0].id);
-           sessionStorage.setItem("logged", "true");
-           window.location.href = "home.html";
-         } else {
-             alert("Usuario no encontrado");
-         }
-       });
-})
+    fetch("https://nodetest-p2ot.onrender.com/login", {
+      method: "POST",
+      body: JSON.stringify({
+        nombre: username,
+        contrasena: password,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.status == "ok") {
+          sessionStorage.setItem("userId", data.body[0].id);
+          sessionStorage.setItem("logged", "true");
+          window.location.href = "home.html";
+        } else {
+          alert("Usuario no encontrado");
+        }
+      });
+  });
 });
-
-
