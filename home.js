62fcc7b28d2c7bec4ce1e25f6f7203918362472b
Owen Arcega <owenarcega11@gmail.com>
2024-11-19 08:19:11 -0600

    update

--- home.js
+++ home.js
@@ -1,13 +1,14 @@
 document.addEventListener("DOMContentLoaded", () => {
-    if (sessionStorage.getItem("logged") != "true") {
-        alert("Por favor inicie sesión.")
-        window.location = "pruebalogin.html";
-    }
+  if (sessionStorage.getItem("logged") != "true") {
+    alert("Por favor inicie sesión.");
+    window.location = "pruebalogin.html";
+  }
 
-    const menuToggle = document.getElementById("menu-toggle");
-    const menuList = document.getElementById("menu-list");
-    
-    menuToggle.addEventListener("click", () => {
-      menuList.classList.toggle("active");
-    });
-})
+  const menuToggle = document.getElementById("menu-toggle");
+  const menuList = document.getElementById("menu-list");
+  console.log(sessionStorage.getItem("userId"));
+
+  menuToggle.addEventListener("click", () => {
+    menuList.classList.toggle("active");
+  });
+});
